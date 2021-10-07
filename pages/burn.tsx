97bea6f865c46cf1f94c4aab8cc4c03a0723b9e4
yeabu369 <yeabsdebe@gmail.com>
2021-10-07 21:20:30 +0300

    Added mint and burn pages

---
+++ pages/burn.tsx
@@ -0,0 +1,9 @@
+import { Panel } from 'components';
+
+export default function Burn() {
+  return (
+    <Panel>
+      <div className="flex justify-center text-gray-300">🔥 Burn Coming soon</div>
+    </Panel>
+  );
+}
---
+++ pages/mint.tsx
@@ -0,0 +1,9 @@
+import { Panel } from 'components';
+
+export default function Mint() {
+  return (
+    <Panel>
+      <div className="flex justify-center text-gray-300">🔸 Minting Coming soon</div>
+    </Panel>
+  );
+}
