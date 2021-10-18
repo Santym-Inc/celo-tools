de1e1e59e43d7fdedc7fb2354f232500a39a7bb2
yeabu369 <yeabsdebe@gmail.com>
2021-10-18 13:30:39 +0300

    Removed lend page

--- pages/lend.tsx
+++
@@ -1,9 +0,0 @@
-import { Panel } from 'components';
-
-export default function Lend() {
-  return (
-    <Panel>
-      <div className="flex justify-center text-gray-300">Coming soon</div>
-    </Panel>
-  );
-}
