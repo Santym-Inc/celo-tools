7deaaab9905290871fcf3579679aa9132f8e48d7
yeabu369 <yeabsdebe@gmail.com>
2021-10-18 13:16:46 +0300

    Deleted swap page

--- pages/swap.tsx
+++
@@ -1,9 +0,0 @@
-import { Panel } from 'components';
-
-export default function Swap() {
-  return (
-    <Panel>
-      <div className="flex justify-center text-gray-300">Coming soon</div>
-    </Panel>
-  );
-}
