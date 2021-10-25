c7f29082886484d958448e5900ff1b4cdd05ee1f
yeabu369 <yeabsdebe@gmail.com>
2021-10-25 11:19:23 +0300

    Renamed Trade to Swap

--- pages/trade.tsx
+++
@@ -1,9 +0,0 @@
-import { Panel } from 'components';
-
-export default function Trade() {
-  return (
-    <Panel>
-      <div className="flex justify-center text-gray-300">Trade</div>
-    </Panel>
-  );
-}
