738a377ab7bbe4d71d5c5464660ef771b2c63026
yeabu369 <yeabsdebe@gmail.com>
2021-10-25 11:19:40 +0300

    Create Swap page

---
+++ pages/swap.tsx
@@ -0,0 +1,24 @@
+import { Panel } from 'components';
+
+export default function Trade() {
+  return (
+    <>
+      <Panel>
+        <div>
+          <h3 className="text-lg font-medium leading-6 text-gray-200">Trade</h3>
+          <h2 className="text-sm font-small text-gray-300">cKSH/cETB</h2>
+          <br />
+          <h2 className="text-md font-medium text-gray-300">0.43</h2>
+        </div>
+      </Panel>
+
+      <Panel>
+        <h3 className="text-gray-200">Swap here</h3>
+
+        <div className="">
+          
+        </div>
+      </Panel>
+    </>
+  );
+}
